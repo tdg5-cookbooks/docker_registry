043f67bc7b9a852a0411596967da768895b85a26
Danny Guinther <dannyguinther@gmail.com>
2015-12-26 09:11:07 -0500

    Use 2 tag

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default.docker_registry.port_map = "5000:5000"
 default.docker_registry.repo = "registry"
 default.docker_registry.restart_policy = "always"
-default.docker_registry.tag = "latest"
+default.docker_registry.tag = "2"
